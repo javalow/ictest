a0de64bca0e01851d7170d4e67614d64931b3e4b
javalow <jeremy@avalow.com>
2018-09-26 14:10:04 -0700

    intercom updates & additional calls

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -9,16 +9,29 @@ import { Intercom } from '@ionic-native/intercom';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController, private intercom: Intercom) {
+
+
+
+  constructor(public navCtrl: NavController, public intercom: Intercom) {}
+  ionViewDidLoad() {
+    this.intercom.displayConversationsList();
+      console.log('intercom display conversation list on load');
 
   }
+
   launch(){
-    this.intercom.setLauncherVisibility('VISIBLE');
+    this.intercom.displayMessenger();
+    console.log('intercom display messenger launched');
 }
   unread() {
     this.intercom.unreadConversationCount();
   }
   visible() {
-    this.intercom.displayMessenger();
+    this.intercom.displayMessageComposer();
+    console.log('intercom display message composer launched');
   }
+  conversations() {
+  this.intercom.displayConversationsList();
+    console.log('intercom display conversation list');
+}
 }
